ef4df3ddda5f8ce587e54656549b1d1cc1233a4d
Paterne <diedhioupaterne279@gmail.com>
2026-09-10 15:17:38 +0000

    feat: Create a new listing modal design ended

--- src/components/modals/CreatePropertyModal.tsx
+++ src/components/modals/CreatePropertyModal.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import Modal from "./Modal";
 import { useCreatePropertyModalStore } from "@/app/store/useCreatePropertyModalStore";
 import Button from "../ui/Button";
 import { propertyTypes } from "@/constants/PropertyTypes";
 import PropertyTypeCard from "../properties/PropertyTypeCard";
+import Input from "../ui/Input";
+import Counter from "../properties/Counter";
+import ImageUpload from "../properties/ImageUpload";
 
 const STEPS = {
   TYPE: 0,
@@ -24,6 +27,21 @@ export default function CreatePropertyModal() {
   const [loading, setLoading] = useState(false);
 
   const [propertyType, setPropertyType] = useState("");
+
+  const [location, setLocation] = useState("");
+  const [address, setAddress] = useState("");
+  const [bedrooms, setBedrooms] = useState(1);
+  const [bathrooms, setBathrooms] = useState(1);
+  const [parkingSpaces, setParkingSpaces] = useState(0);
+  const [area, setArea] = useState("");
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [image, setImage] = useState<null | File>(null);
+  const [preview, setPreview] = useState<null | string>(null);
+  const [listingType, setListingType] = useState<"rent" | "sale">("sale");
+  const [price, setPrice] = useState("");
 
   const stepTitle = () => {
     switch (step) {
@@ -45,6 +63,11 @@ export default function CreatePropertyModal() {
   };
 
   const createListing = async () => {};
+
+  const handleImageChange = (file: File) => {
+    setImage(file);
+    setPreview(URL.createObjectURL(file));
+  };
 
   return (
     <Modal onClose={close} isOpen={isOpen} title="Create a new listing">
@@ -74,6 +97,110 @@ export default function CreatePropertyModal() {
                 key={item.slug}
               />
             ))}
+          </div>
+        )}
+        {step === STEPS.LOCATION && (
+          <div className="space-y-6 w-full">
+            <Input
+              name="location"
+              label="Location"
+              value={location}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setLocation(e.target.value)
+              }
+            />
+            <Input
+              name="address"
+              label="Address"
+              value={address}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setAddress(e.target.value)
+              }
+            />
+          </div>
+        )}
+
+        {step === STEPS.DETAILS && (
+          <div className="space-y-4">
+            <Counter
+              title="Bedrooms"
+              subTitle="How many bedrooms"
+              value={bedrooms}
+              onChange={setBedrooms}
+            />
+            <Counter
+              title="Bathrooms"
+              subTitle="How many bathrooms"
+              value={bathrooms}
+              onChange={setBathrooms}
+            />
+            <Counter
+              title="Parking Spaces"
+              value={parkingSpaces}
+              onChange={setParkingSpaces}
+              subTitle="How many parking spaces"
+            />
+
+            <Input
+              name="area"
+              label="Property Area (sqft)"
+              type="number"
+              value={area}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setArea(e.target.value)
+              }
+            />
+          </div>
+        )}
+
+        {step === STEPS.FEATURE && (
+          <div className="space-y-6">
+            <Input
+              name="title"
+              label="Property Title"
+              value={title}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setTitle(e.target.value)
+              }
+            />
+
+            <Input
+              name="description"
+              label="Description"
+              value={description}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setDescription(e.target.value)
+              }
+            />
+          </div>
+        )}
+
+        {step === STEPS.IMAGE && (
+          <ImageUpload preview={preview} onChange={handleImageChange} />
+        )}
+
+        {step === STEPS.PRICING && (
+          <div className="space-y-6">
+            <select
+              value={listingType}
+              onChange={(e) =>
+                setListingType(e.target.value as "sale" | "rent")
+              }
+              className="h-13 w-full rounded-2xl border border-black/10
+              p-4"
+            >
+              <option value="rent">For Rent</option>
+              <option value="sale">For Sale</option>
+            </select>
+            <Input
+              name="price"
+              label={listingType === "sale" ? "Sale Price" : "Monthly Rent"}
+              type="number"
+              value={price}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPrice(e.target.value)
+              }
+            />
           </div>
         )}
       </div>
